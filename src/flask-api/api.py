10419b7194efc86c83d6946cc942d130291034f0
DBCsTOrM jose <pernetlouis@yahoo.com>
2023-03-24 11:17:27 +0000

    TP6/7LouisPERNET

--- src/flask-api/api.py
+++ src/flask-api/api.py
@@ -1,7 +1,37 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, API!"
+# Configuration de la base de données
+app.config['MYSQL_USER'] = 'louis'
+app.config['MYSQL_PASSWORD'] = 'louis'
+app.config['MYSQL_DB'] = 'identity'
+app.config['MYSQL_HOST'] = 'localhost'
+
+# Initialisation de la connexion à la base de données
+mysql = MySQL(app)
+
+@app.route("/login")
+def login():
+    # Récupération des informations de connexion envoyées depuis website.py
+    username = request.args.get('username')
+    password = request.args.get('password')    
+    
+    # Connexion à la base de données
+    cur = mysql.connection.cursor()
+    
+    # Récupération de l'utilisateur correspondant au nom d'utilisateur
+    cur.execute("SELECT * FROM users WHERE username=%s", (username,))
+    user = cur.fetchone()
+    
+    # Vérification du mot de passe
+    if user and user[2] == password:
+        # Utilisateur authentifié
+        response = {'authenticated': True}
+    else:
+        # Nom d'utilisateur ou mot de passe incorrect
+        response = {'authenticated': False}
+    
+    # Retourne la réponse au format JSON à l'application Flask appelante
+    return jsonify(response)
--- src/flask-website/website.py
+++ src/flask-website/website.py
@@ -1,8 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
-def start(): 
-    return render_template('start.html')
-    
+def start():
+    return render_template('login.html')
+
+@app.route("/layout")
+def layout():
+    return render_template('layout.html')
+
+@app.route("/login")
+def login():
+    # Récupération du formulaire de connexion
+    username = request.args.get('username')
+    password = request.args.get('password')
+
+    # Requête vers l'API pour vérification des informations
+    r = requests.get('http://localhost:5000/login', params={'username': username, 'password': password})
+    response = r.json()
+
+    # Si l'authentification est réussie, redirection vers la page "layout"
+    if response['authenticated']:
+        return redirect(url_for('layout'))
+    # Sinon, retour à la page de connexion
+    else:
+        return redirect(url_for('start'))
+
